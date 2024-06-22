b98954c0a1b1ef524264f315be7801a99fa82933
Alejandro Raya <alejandroraya09@hotmail.com>
2024-06-21 23:41:48 -0600

    MAQUETACIION RESULTADO

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
 export class InicioComponent {
+
+  private router = inject(Router)
+
+  edad: number = 25;
+  peso: number = 60;
+  altura: number = 170;
+  sexo: string = 'Masculino';
+
+  cambiarAltura(event: any) {
+    this.altura = event.target.value;
+  }
+
+  masculino() {
+    this.sexo = 'Masculino';
+  }
+  femenino() {
+    this.sexo = 'Femenino';
+  }
+
+  calcularBMI(){
+    this.router.navigate(['/resultado'])
+  }
 
 }
